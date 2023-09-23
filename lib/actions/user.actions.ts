6907746280ce97829fd3d899bf314a504f701486
mateuszstasak <mstasaq@gmail.com>
2023-09-23 23:52:16 +0200

    fix user fetching

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -10,59 +10,56 @@ import { FilterQuery, SortOrder } from 'mongoose';
 
 export async function fetchUser(userId: string) {
   try {
-      connectToDB();
+    connectToDB();
 
-      return await User
-      .findOne({ id: userId })
-      .populate({
-          path: 'communities',
-          model: Community
-      });
+    return await User.findOne({ id: userId }).populate({
+      path: "communities",
+      model: Community,
+    });
   } catch (error: any) {
-      throw new Error(`Failed to fetch user: ${error.message}`)
+    throw new Error(`Failed to fetch user: ${error.message}`);
   }
 }
 
-
 interface Params {
-    userId: string;
-    username: string;
-    name: string;
-    bio: string;
-    image: string;
-    path: string;
-  }
+  userId: string;
+  username: string;
+  name: string;
+  bio: string;
+  image: string;
+  path: string;
+}
 
 export async function updateUser({
-    userId,
-    bio,
-    name,
-    path,
-    username,
-    image
-  }: Params): Promise<void> {
-    try {
-      connectToDB();
-  
-      await User.findOneAndUpdate(
-        { id: userId },
-        {
-          username: username.toLowerCase(),
-          name,
-          bio,
-          image,
-          onboarded: true,
-        },
-        { upsert: true }
-      );
-  
-      if (path === '/profile/edit') {
-        revalidatePath(path);
-      }
-    } catch (error: any) {
-      throw new Error(`Failed to create/update user: ${error.message}`);
+  userId,
+  bio,
+  name,
+  path,
+  username,
+  image,
+}: Params): Promise<void> {
+  try {
+    connectToDB();
+
+    await User.findOneAndUpdate(
+      { id: userId },
+      {
+        username: username.toLowerCase(),
+        name,
+        bio,
+        image,
+        onboarded: true,
+      },
+      { upsert: true }
+    );
+
+    if (path === "/profile/edit") {
+      revalidatePath(path);
     }
+  } catch (error: any) {
+    throw new Error(`Failed to create/update user: ${error.message}`);
   }
+}
 
 
   export async function fetchUserThreads(userId: string) {
